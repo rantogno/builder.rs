1b0739810295853c85bbff09294e74e8ecda81d5
Rafael Antognolli <rafael.antognolli@intel.com>
2017-12-12 14:54:18 -0800

    Adding clap crate.
    Also use it a little bit.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Builder script")
+        .version("0.1")
+        .author("Rafael Antognolli <antognolli@gmail.com>")
+        .about("Builds mesa, xserver, weston...")
+        .arg(Arg::with_name("v")
+             .short("v")
+             .help("Verbose output - prints stdout and stderr"))
+        .arg(Arg::with_name("PACKAGE")
+             .help("List of packages to build")
+             .index(1)
+             .multiple(true))
+        .get_matches();
+
+    if matches.is_present("v") {
+        println!("Printing verbose output...")
+    } else {
+        println!("Printing normally...")
+    }
 }
