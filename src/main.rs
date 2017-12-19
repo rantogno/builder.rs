c1bf0ec641791df98d21811a5452af0c605dccbb
Rafael Antognolli <rafael.antognolli@intel.com>
2017-12-18 17:41:42 -0800

    Add more complexity.
     - List of packages parsed and compared with default one.
     - Added "install" subcommand, and some basic parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,87 @@
 extern crate clap;
-use clap::{Arg, App};
+use clap::{Arg, App, SubCommand};
+
+struct Config {
+    verbose: bool,
+    cmd: Command,
+}
+
+struct InstallOptions {
+    force: bool,
+}
+
+enum Command {
+    Install{options: InstallOptions},
+    Clean,
+    Invalid,
+}
+
+static PACKAGES: &'static [&str] = &[
+    "libdrm",
+    "mesa"
+];
+
+// Check pkg list for invalid items
+//  - returns list if valid, and None if some pkg names are not valid.
+fn parse_pkg_list(pkglist: Vec<&str>) -> Option<Vec<&str>> {
+    let mut notfound = false;
+
+    for pkg in &pkglist {
+        if !PACKAGES.contains(&pkg) {
+            println!("Invalid pkgname: {}", pkg);
+            notfound = true;
+        }
+    }
+
+    match notfound {
+        true => None,
+        false => Some(pkglist),
+    }
+}
+
+// Get pkg list from ArgMatches.
+//  - returns Some(values) if pkglist is valid or the default list if
+//    no list is provided.
+//  - returns None if the list is invalid
+fn get_pkg_list<'a>(matches: &'a clap::ArgMatches) -> Option<Vec<&'a str>> {
+    let pkglist = match matches.values_of("PACKAGE") {
+        Some(values) => parse_pkg_list(values.collect()),
+        None => Some(PACKAGES.to_vec()),
+    };
+
+    pkglist
+}
+
+fn parse_install(matches: &clap::ArgMatches) -> InstallOptions {
+    match get_pkg_list(matches) {
+        Some(pkglist) => {
+            for p in pkglist {
+                println!("pkg: {}", p);
+            }
+        }
+        None => println!("Invalid package in list... aborting.")
+    }
+    InstallOptions{force: false}
+}
+
+fn parse_matches(matches: &clap::ArgMatches) -> Config {
+    let v = if matches.is_present("v") {
+        true
+    } else {
+        false
+    };
+    Config{
+        verbose: v,
+        cmd: match matches.subcommand() {
+            ("install", Some(sub_m)) => {
+                Command::Install{options: parse_install(&sub_m)}
+            }
+            ("clean", Some(_)) => Command::Clean,
+            (&_, Some(_)) => Command::Invalid,
+            (&_, None) => Command::Invalid,
+        },
+    }
+}
 
 fn main() {
     let matches = App::new("Builder script")
@@ -9,22 +91,14 @@ fn main() {
         .arg(Arg::with_name("v")
              .short("v")
              .help("Verbose output - prints stdout and stderr"))
-        .arg(Arg::with_name("PACKAGE")
-             .help("List of packages to build")
-             .index(1)
-             .multiple(true))
+        .subcommand(SubCommand::with_name("install")
+                    .about("Build and install package")
+                    .arg(Arg::with_name("PACKAGE")
+                         .help("List of packages to build")
+                         .index(1)
+                         .multiple(true)))
         .get_matches();
 
-    if matches.is_present("v") {
-        println!("Printing verbose output...")
-    }
-
-    let pkglist = match matches.values_of("PACKAGE") {
-        Some(values) => values.collect(),
-        None => vec!["all"],
-    };
-
-    for pkg in pkglist {
-        println!("printing pkg: {}", pkg);
-    }
+    let config = parse_matches(&matches);
+    println!("{:?}", config.verbose);
 }
